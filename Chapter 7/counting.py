3b5610272d4dd65370bd20b2f323fe30629c69e5
zhenxuhuang <1105314086@qq.com>
2022-09-06 12:18:42 +0800

    first commit

---
+++ Chapter 7/counting.py
@@ -0,0 +1,7 @@
+# Python编程：从入门到实践
+# 第7章 用户输入和while循环
+# 7.2
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
--- Chapter 7/even_or_odd.py
+++ Chapter 7/even_or_odd.py
@@ -1,3 +1,9 @@
 # Python编程：从入门到实践
 # 第7章 用户输入和while循环
 # 7.1
+number = int(input("Enter a number, and I'll tell you if it's even or odd: "))
+
+if number % 2 == 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
