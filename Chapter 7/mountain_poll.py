8e4fa9c2cba4896bc6559653336ea6966815b08c
zhenxuhuang <1105314086@qq.com>
2022-09-11 22:15:31 +0800

    9.11

--- Chapter 7/mountain_poll.py
+++ Chapter 7/mountain_poll.py
@@ -8,3 +8,11 @@ while polling_active:
     response = input("Which mountain would you like to climb someday? ")
 
     responses[name] = response
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name} would like to climb {response}.")
